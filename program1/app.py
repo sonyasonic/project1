3f07dad701912179eefc63f5499c0b50550262bd
sonyasonic <lessiksofa@gmail.com>
2024-11-23 23:23:18 +0300

    Update app.py

--- program1/app.py
+++ program1/app.py
@@ -1,31 +1,10 @@
 from datetime import datetime, timedelta
-from mymodule_for_project import plane
+from utils import plane
 
 
 def greet():
     name = input("Введите имя: ")
     print("Бу," + name + "! Испугался? Не бойся, я расскажу тебе, как ты спал :)")
-
-def time_difference():
-    time_format = "%H:%M"
-
-    while True:
-        sleep_str = input("Введи время засыпания (часы:минуты): ")
-        wakeup_str = input("Введи время пробуждения (часы:минуты): ")
-
-        try:
-            time1 = datetime.strptime(sleep_str, time_format)
-            time2 = datetime.strptime(wakeup_str, time_format)
-
-
-            if time2 < time1:
-                time2 += timedelta(days=1)
-            else:
-                timediff = time2 - time1
-            return timediff
-
-        except ValueError:
-            print("Ошибка :(", "Введи правильный формат времени (часы:минуты)!", sep = "\n")
 
 
 if __name__ == "__main__":  # не особо поняла, зачем она, но в гугле сказали при импорте модулей с ней проще
